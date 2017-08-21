112d3599f8478efc9f8e361a09c0b2aca328f3a2
zhangli <zl123>
2017-08-21 17:08:36 +0800

    添加水波纹

--- app/src/main/java/demo/zl/com/MyDrawViewActivity.java
+++ app/src/main/java/demo/zl/com/MyDrawViewActivity.java
@@ -13,6 +13,7 @@ import android.widget.RelativeLayout;
 
 public class MyDrawViewActivity extends BaseActivity{
 
+    private MyWeaveView myWeaveView;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +24,15 @@ public class MyDrawViewActivity extends BaseActivity{
     }
 
     private void init() {
+        this.myWeaveView = (MyWeaveView) findViewById(R.id.myweave);
+
+        myWeaveView.setOnWaveAnimationListener(new MyWeaveView.OnWaveAnimationListener() {
+            @Override
+            public void OnWaveAnimation(float y) {
+//                lp.setMargins(0,0,0,(int)y+2);
+//                imageView.setLayoutParams(lp);
+            }
+        });
 
     }
 
